be67d659f9af57862509317c3411b62866ecd101
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-12-28 13:27:16 -0300

    Created the Activity Skills and used the Explicit Intent to navigate from home page to skills page

--- app/src/main/java/com/novelitech/portfoliosimplekotlin/MainActivity.kt
+++ app/src/main/java/com/novelitech/portfoliosimplekotlin/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.novelitech.portfoliosimplekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonSkills: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSkills = findViewById(R.id.buttonSkills)
+
+        buttonSkills.setOnClickListener {
+            intent = Intent(this, SkillsActivity::class.java)
+
+            startActivity(intent)
+        }
+
     }
 }
